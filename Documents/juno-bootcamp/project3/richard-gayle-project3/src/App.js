597e6a4145827fcf2e31cd62714d66b378ce2c29
Richard <richardcgayle@gmail.com>
2021-07-26 12:31:43 -0400

    first commit

---
+++ Documents/juno-bootcamp/project3/richard-gayle-project3/src/App.js
@@ -0,0 +1,146 @@
+import './App.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import RecipeCard from './RecipeCard'
+import Footer from './Footer'
+
+
+
+
+
+function App() {
+
+  const [ recipeArray, setRecipeArray ] = useState([]);
+  const [ userChoice, setUserChoice ] = useState("");
+  const [ query, setQuery ] = useState("")
+
+
+
+  useEffect( () => {
+    const apiKey = `accc4d584071e2125321b1a0c56a89b3`
+    const apiId = `9c9f80c0`
+    // const reqUrl = `https://api.edamam.com/search`
+    // const input = 'chicken'
+
+    axios({
+      url: `https://api.edamam.com/search?q=${query}&app_id=${apiId}&app_key=${apiKey}`,
+      // reqUrl: reqUrl,
+      //           headers: {
+      //               "Content-Type": "application/json"
+      //           },
+      params: {
+        q: query,
+        apiId: apiId,
+        apiKey: apiKey
+      }
+    }).then( (responseData) => {
+      // once we get the data back, we want to UPDATE our state that holds the data, so we can use it in our JSX
+      console.log(recipeArray)
+      console.log(responseData.data.hits);
+      setRecipeArray(responseData.data.hits)
+
+
+
+      // on change of query this value should change, but only does on second query change
+      console.log(recipeArray);
+    })
+
+  }, [query])
+
+
+  const handleChange = (event) => {
+
+    setUserChoice(event.target.value)
+    
+  }
+
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    setQuery(userChoice);
+    
+    setUserChoice("")
+    // checking if results array is empty
+    // if(!recipeArray.length) {
+      //   console.log('try again');
+      // }
+      // console.log(recipeArray.length === 0);
+      // I dont understand why the recipe array has no value at this point
+      
+       
+      console.log(recipeArray.length);
+    }
+
+    const modalAlert = () => {
+      // if(recipeArray.length === 0) {
+      //   console.log('herehere');
+      // }
+    }
+
+    modalAlert();
+
+    
+
+
+
+
+  return (
+    <div className="App">
+      <header>
+        <div className="wrapper">
+          <h1>Recipe Hunter</h1>
+          <h2>We've got everything you need</h2>
+        </div>
+      </header>
+      
+      <main className="wrapper">
+
+        <form action="" onSubmit={handleSubmit}>
+          <div className="label-search-container">
+          <label htmlFor="userFoodChoice">Choose your ingredients</label>
+          <input type="text" 
+          id="userFoodChoice"
+          placeholder="Chicken, Asparagus"
+          onChange={handleChange}
+          value={userChoice}
+          required
+          />
+          </div>
+        </form>
+
+      <section className="user-recipe">
+
+        <ul>
+      {
+        recipeArray.map( (recipeObj, index) => {
+          return (
+                <li key={index}>
+                <RecipeCard 
+                recipeObj={recipeObj}
+                />
+                </li> 
+            )
+          })
+          
+      }
+        </ul>
+
+      </section>
+
+      </main>
+
+      <Footer />
+
+
+
+    </div>
+  );
+}
+
+export default App;
+
+// error handling
+// I want modal to show only after submit has been hit AND array length is STILL 0
+  // if search put in and the array/page is still empty ie no results then create modal box 
+
